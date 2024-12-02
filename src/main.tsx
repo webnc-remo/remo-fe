da9f18418ef8123686fc13dce51d4a85efe10301
tantran <tantran.300803@gmail.com>
2024-12-02 16:25:00 +0700

    Fix list-style-type in breadcrumb and update font-family in Gilroy.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './App';
+import App from "./App";
 
-import './tailwind.css';
+import "./tailwind.css";
 
-const container = document.getElementById('root') as HTMLElement;
+const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
