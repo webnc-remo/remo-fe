b9718f46bc7f3c7cb617b46b9ad906e1ea40750f
Le Sang <lesangbt99@gmail.com>
2024-12-30 19:50:24 +0700

    chore: footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,8 +4,11 @@ export const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</p>
+        <p>
+          &copy; {new Date().getFullYear()} Your Company Name. All rights
+          reserved.
+        </p>
       </div>
     </footer>
   );
-}; 
+};
